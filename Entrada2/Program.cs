eaf0dd5cb0dda53bb70ec696b323ba505e89cbfb
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-11-28 11:34:24 -0300

    entrada de dados

---
+++ Entrada2/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Entrada2
+{
+  public static void Main(string[] args)
+  {
+    Console.Write("Entre com seu nome completo: ");
+    string nome = Console.ReadLine();
+
+    Console.Write("Quantos quartos tem em sua casa? ");
+    int quartos = int.Parse(Console.ReadLine());
+
+    Console.Write("Quanto você ganha? ");
+    float price = float.Parse(Console.ReadLine());
+
+    Console.WriteLine(nome);
+    Console.WriteLine(quartos);
+    Console.WriteLine(price);
+  }
+}
